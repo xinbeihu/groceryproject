e6ffb1650a3b13f79bcddf4f3bf2e6eb062d4e3f
Xinbei Hu <tkgs.hu.xinbei@gmail.com>
2020-03-08 23:47:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# groceryproject
+ 
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="headerStyle">
+    <header>
+        <h1>{{ msg }}</h1>
+    </header>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data(){
+    return{
+        msg: 'Grocery List'
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header{
+    background:rgb(122, 63, 100);
+    padding:10px;
+    border-style: solid;
+    border-color:black;
+    border-width: 2px;
+}
+h1{
+    color:ivory
+}
+</style>
