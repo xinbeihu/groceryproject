10f53807a220606d3c141874e1f7aa772c66c7e7
Xinbei Hu <tkgs.hu.xinbei@gmail.com>
2020-03-10 10:16:50 +0800

    added firebase api

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,16 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.firestore();
-  export default database;
+    apiKey: "AIzaSyCiWjsSKmeSCZheWyeJIaaP7JowhZcpI5o",
+    authDomain: "bt3103-1e0dd.firebaseapp.com",
+    databaseURL: "https://bt3103-1e0dd.firebaseio.com",
+    projectId: "bt3103-1e0dd",
+    storageBucket: "bt3103-1e0dd.appspot.com",
+    messagingSenderId: "924846115008",
+    appId: "1:924846115008:web:95abc0789873ac1bf2c525",
+    measurementId: "G-XS3M2T0C8Z"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.firestore();
+export default database;
